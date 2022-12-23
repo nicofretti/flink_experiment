70f3787c897be0b9b0b593ae84d1c1c2257f5ecb
nicof <34170904+nicofretti@users.noreply.github.com>
2022-12-23 13:44:08 +0100

    Fixing timer

--- src/data_expo_queries/src/main/java/org/data/expo/BestDayOfWeek.java
+++ src/data_expo_queries/src/main/java/org/data/expo/BestDayOfWeek.java
@@ -1,21 +1,25 @@
 package org.data.expo;
 
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.common.serialization.Encoder;
+import org.apache.flink.api.common.state.ReducingState;
+import org.apache.flink.api.common.state.ReducingStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.common.typeutils.base.LongSerializer;
 import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.configuration.MemorySize;
-import org.apache.flink.connector.file.sink.FileSink;
-import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
-import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.PurgingTrigger;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
+import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 
 import java.time.Duration;
 
@@ -23,14 +27,17 @@ import java.time.Duration;
 public class BestDayOfWeek {
   public static void main(String[] args) throws Exception {
     // Debugging:
-    // StreamExecutionEnvironment env = LocalStreamEnvironment.createLocalEnvironment();
+    StreamExecutionEnvironment env = LocalStreamEnvironment.createLocalEnvironment();
     // DataStream<String> data_stream = env.fromCollection(DataExpoDebug.example);
     // Deployment: for docker replace localhost -> host.docker.internal
-    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    // StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     DataStream<String> data_stream = env.socketTextStream("localhost", 8888);
     // Create the result stream
     SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> result =
         data_stream
+            .assignTimestampsAndWatermarks(
+                WatermarkStrategy.<String>forBoundedOutOfOrderness(Duration.ofSeconds(1))
+                    .withTimestampAssigner((event, timestamp) -> timestamp))
             .flatMap(
                 (FlatMapFunction<String, DataExpoRow>)
                     (value, out) -> {
@@ -49,30 +56,128 @@ public class BestDayOfWeek {
                 Types.TUPLE(Types.INT, Types.INT, Types.INT))
             .keyBy(value -> value.f0) // Assign partitions by day of week
             // Deployment: every 10 seconds there is a calculation
-            //.window(TumblingEventTimeWindows.of(Time.seconds(2)))
+            .window(TumblingEventTimeWindows.of(Time.seconds(2)))
             // Debug window processed instantly
-            .window(EventTimeSessionWindows.withGap(Time.seconds(1)))
+            // .window(EventTimeSessionWindows.withGap(Time.seconds(1)))
+            .trigger(PurgingTrigger.of(TimerTrigger.of(Time.seconds(2))))
+            // .trigger(ProcessingTimeTrigger.create())
             .reduce(
                 (ReduceFunction<Tuple3<Integer, Integer, Integer>>)
                     (i, j) -> new Tuple3<>(i.f0, i.f1 + j.f1, i.f2 + j.f2));
+    result.print();
     // Creating the sink of output file
-    final FileSink<Tuple3<Integer, Integer, Integer>> sink =
-        FileSink.forRowFormat(
-                new Path("output"),
-                (Encoder<Tuple3<Integer, Integer, Integer>>)
-                    (element, stream) ->
-                        stream.write(
-                            (String.format(
-                                    "%d,%.2f\n", element.f0, ((double) element.f1 / element.f2))
-                                .getBytes())))
-            .withRollingPolicy(
-                DefaultRollingPolicy.builder()
-                    .withInactivityInterval(Duration.ofSeconds(2))
-                    .withMaxPartSize(MemorySize.ofMebiBytes(1024))
-                    .build())
-            .build();
-    // Writing the result, the parallelism is 1 to avoid multiple files
-    result.sinkTo(sink).setParallelism(1);
+    // final FileSink<Tuple3<Integer, Integer, Integer>> sink =
+    //    FileSink.forRowFormat(
+    //            new Path("output"),
+    //            (Encoder<Tuple3<Integer, Integer, Integer>>)
+    //                (element, stream) ->
+    //                    stream.write(
+    //                        (String.format(
+    //                                "%d,%.2f\n", element.f0, ((double) element.f1 / element.f2))
+    //                            .getBytes())))
+    //        .withRollingPolicy(
+    //            DefaultRollingPolicy.builder()
+    //                .withInactivityInterval(Duration.ofSeconds(2))
+    //                .withMaxPartSize(MemorySize.ofMebiBytes(1024))
+    //                .build())
+    //        .build();
+    //// Writing the result, the parallelism is 1 to avoid multiple files
+    // result.sinkTo(sink).setParallelism(1);
     env.execute("BestDayOfWeek");
+  }
+  // override the ProcessingTimeTrigger behavior
+  public static class TimerTrigger<W extends Window> extends Trigger<Object, W> {
+    private static final long serialVersionUID = 1L;
+    private final long interval;
+    private final ReducingStateDescriptor<Long> stateDesc;
+
+    private TimerTrigger(long winInterValMills) { // window
+      this.stateDesc =
+          new ReducingStateDescriptor("fire-time", new TimerTrigger.Min(), LongSerializer.INSTANCE);
+      this.interval = winInterValMills;
+    }
+
+    public static <W extends Window> TimerTrigger<W> of(Time interval) {
+      return new TimerTrigger(interval.toMilliseconds());
+    }
+
+    public TriggerResult onElement(Object element, long timestamp, W window, TriggerContext ctx)
+        throws Exception {
+      if (window.maxTimestamp() <= ctx.getCurrentWatermark()) {
+        // if the watermark is already past the window fire immediately
+        return TriggerResult.FIRE;
+      }
+      long now = System.currentTimeMillis();
+      ReducingState<Long> fireTimestamp = (ReducingState) ctx.getPartitionedState(this.stateDesc);
+      if (fireTimestamp.get() == null) {
+        long time = Math.max(timestamp, window.maxTimestamp()) + interval;
+        if (now - window.maxTimestamp() > interval) { // fire late
+          time = (now - now % 1000) + interval - 1;
+        }
+        ctx.registerProcessingTimeTimer(time);
+        fireTimestamp.add(time);
+        return TriggerResult.CONTINUE;
+      } else {
+        return TriggerResult.CONTINUE;
+      }
+    }
+
+    public TriggerResult onEventTime(long time, W window, TriggerContext ctx) throws Exception {
+      if (time == window.maxTimestamp()) {
+        return TriggerResult.FIRE;
+      }
+      return TriggerResult.CONTINUE;
+    }
+
+    public TriggerResult onProcessingTime(long time, W window, TriggerContext ctx)
+        throws Exception {
+      ReducingState<Long> fireTimestamp = (ReducingState) ctx.getPartitionedState(this.stateDesc);
+      if (((Long) fireTimestamp.get()).equals(time)) {
+        fireTimestamp.clear();
+        long maxTimestamp = Math.max(window.maxTimestamp(), time); // maybe useless
+        if (maxTimestamp == time) {
+          maxTimestamp = time + this.interval;
+        }
+        fireTimestamp.add(maxTimestamp);
+        ctx.registerProcessingTimeTimer(maxTimestamp);
+        return TriggerResult.FIRE;
+      } else {
+        return TriggerResult.CONTINUE;
+      }
+    }
+
+    public void clear(W window, TriggerContext ctx) throws Exception {
+      ReducingState<Long> fireTimestamp = (ReducingState) ctx.getPartitionedState(this.stateDesc);
+      long timestamp = (Long) fireTimestamp.get();
+      ctx.deleteProcessingTimeTimer(timestamp);
+      fireTimestamp.clear();
+    }
+
+    public boolean canMerge() {
+      return true;
+    }
+
+    public void onMerge(W window, OnMergeContext ctx) {
+      ctx.mergePartitionedState(this.stateDesc);
+    }
+
+    @VisibleForTesting
+    public long getInterval() {
+      return this.interval;
+    }
+
+    public String toString() {
+      return "TimerTrigger(" + this.interval + ")";
+    }
+
+    private static class Min implements ReduceFunction<Long> {
+      private static final long serialVersionUID = 1L;
+
+      private Min() {}
+
+      public Long reduce(Long value1, Long value2) throws Exception {
+        return Math.min(value1, value2);
+      }
+    }
   }
 }
--- src/tools/data_expo_socket.py
+++ src/tools/data_expo_socket.py
@@ -5,24 +5,25 @@ import pwn
 
 if __name__ == "__main__":
     files = ["../datasets/2005.csv", "../datasets/2006.csv"]
-    batch_size = 500
+    # Parameters
+    batch_size = 2000000
+    max_response = 10
     current_dir = os.path.dirname(os.path.realpath(__file__))
     # Init server
     server = pwn.listen(8888)
     # Wait for connection
     client = server.wait_for_connection()
-    # Close connections
-    count = 0
+    count_response = 0
+    x = ""
     with open(os.path.join(current_dir, files[0]), "r") as f:
         header = f.readline()
         rows = f.readline()
-        while rows != "" and count != 2:
+        while rows != "" and max_response > count_response and x!="n":
             for i in range(batch_size):
                 rows += f.readline()
             client.send(rows.encode())
             rows = f.readline()
-            count+=1
-            time.sleep(2)
-            print("SEND")
+            count_response += 1
+            x = input("Continue? (y/n)")
     client.close()
     server.close()
